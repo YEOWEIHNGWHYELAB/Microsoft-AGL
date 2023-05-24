72ebc728571df299ad5928577caab5ae57028248
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-05-24 13:37:21 +0800

    Good looking graph using same layer sample

--- GraphLayout/Samples/SameLayerSample/Form1.cs
+++ GraphLayout/Samples/SameLayerSample/Form1.cs
@@ -17,11 +17,31 @@ namespace SameLayerSample
             Graph graph = new Graph();
             var sugiyamaSettings = (SugiyamaLayoutSettings)graph.LayoutAlgorithmSettings;
             sugiyamaSettings.NodeSeparation *= 2;
-            graph.AddEdge("A", "B");
-            graph.AddEdge("A", "C");
-            graph.AddEdge("A", "D");
-            //graph.LayerConstraints.PinNodesToSameLayer(new[] { graph.FindNode("A"), graph.FindNode("B"), graph.FindNode("C") });
-            graph.LayerConstraints.AddSameLayerNeighbors(graph.FindNode("A"), graph.FindNode("B"), graph.FindNode("C"));
+
+            graph.AddEdge("Start\n_____________________\nSave as Draft\nCommit", "Draft\n_____________________\nSubmit for Approval\nCommit");
+            graph.AddEdge("Start\n_____________________\nSave as Draft\nCommit", "Pending Approval\n_____________________\nReject\nQuery");
+            graph.AddEdge("Draft\n_____________________\nSubmit for Approval\nCommit", "Pending Approval\n_____________________\nReject\nQuery");
+            graph.AddEdge("Query\n_____________________\nSubmit for Approval\nCancel", "Cancelled\n_____________________\nNo actions required");
+            graph.AddEdge("Query\n_____________________\nSubmit for Approval\nCancel", "Pending Approval\n_____________________\nReject\nQuery");
+            graph.AddEdge("Pending Approval\n_____________________\nReject\nQuery", "Pending Approval\n_____________________\nReject\nQuery");
+            graph.AddEdge("Pending Approval\n_____________________\nReject\nQuery", "Rejected\n_____________________\nNo actions required");
+            graph.AddEdge("Query\n_____________________\nSubmit for Approval\nCancel", "Rejected\n_____________________\nNo actions required");
+            graph.AddEdge("Committed\n_____________________\nReverse", "Reversed\n_____________________\nNo actions required");
+
+            //graph.AddEdge("A", "B");
+            //graph.AddEdge("A", "C");
+            //graph.AddEdge("A", "D");
+
+            //graph.LayerConstraints.PinNodesToSameLayer(new[] { 
+            //    graph.FindNode("Start\n_____________________\nSave as Draft\nCommit"), 
+            //    graph.FindNode("Draft\n_____________________\nSubmit for Approval\nCommit"), 
+            //    graph.FindNode("Pending Approval\n_____________________\nReject\nQuery") });
+
+            //graph.LayerConstraints.AddSameLayerNeighbors(
+            //    graph.FindNode("Start\n_____________________\nSave as Draft\nCommit"), 
+            //    graph.FindNode("Draft\n_____________________\nSubmit for Approval\nCommit"), 
+            //    graph.FindNode("Pending Approval\n_____________________\nReject\nQuery"));
+
             gViewer.Graph = graph;
 
         }
